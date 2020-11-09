1df1d025fe7b76e617c81e92fc08b2d5f6adb8a1
Stewart <0945195609@ICE.DHS.GOV>
2020-11-09 15:54:12 -0500

    controller test

---
+++ src/test/java/org/ryanstewart/objectdetection/controller/ObjectDetectionControllerTest.java
@@ -0,0 +1,49 @@
+package org.ryanstewart.objectdetection.controller;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.io.InputStream;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ryanstewart.objectdetection.model.dto.DetectionResponseDTO;
+import org.ryanstewart.objectdetection.service.ObjectDetectionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(ObjectDetectionController.class)
+@ActiveProfiles("test")
+public class ObjectDetectionControllerTest {
+
+	@Autowired
+	private MockMvc mvc;
+
+	@MockBean
+	private ObjectDetectionService service;
+
+	@Test
+	public void shouldReturnDtoWhenCalledSuccessfully() throws Exception {
+		InputStream is = this.getClass()
+				.getClassLoader()
+				.getResourceAsStream("testimage1.jpg");
+		MockMultipartFile imageFile = new MockMultipartFile("image", "testimage1.jpg", "multipart/form-data", is);
+
+		when(service.detectAll(imageFile)).thenReturn(DetectionResponseDTO.builder().build());
+
+		this.mvc.perform(MockMvcRequestBuilders.multipart("/api/detect").file(imageFile)
+				.contentType("multipart/form-data"))
+				.andExpect(status().isOk());
+	}
+
+	@Test
+	public void hello() {
+	}
+}
